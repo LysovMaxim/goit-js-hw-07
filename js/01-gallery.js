5671e40926bd9d2364b49d388d507765a5796aa9
LysovMaxim <max_maran@icloud.com>
2023-03-02 17:18:39 +0200

    Зробив дві галереї з підключеними бібліотеками

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,52 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery");
+
+const imgs = createImg(galleryItems);
+
+galleryEl.insertAdjacentHTML("afterbegin", imgs);
+
+function createImg(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join("");
+    
+}
+
+galleryEl.addEventListener("click", addModalimg)
+
+function addModalimg(event) {
+    event.preventDefault();
+    if (!event.target.src)
+        return
+    const instance = basicLightbox.create(`
+       <img src = "${event.target.dataset.source}">
+`)
+  instance.show();
+
+  document.addEventListener("keydown", (event)=> {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+});
+}
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const pictures = createImg(galleryItems);
+
+galleryEl.insertAdjacentHTML("afterbegin", pictures);
+
+function createImg(galleryItems) {
+    return galleryItems.map(({preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join("");
+};
+
+var lightbox = new SimpleLightbox('.gallery a',
+    {
+     captionsData: 'alt',
+     captionDelay: 250,
+     scrollZoom: false, });
